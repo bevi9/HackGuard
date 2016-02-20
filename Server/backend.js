3ea9c1d6f0f29467f992a81fa25dc800a99faeb3
Berta <berta.viader@est.fib.upc.edu>
2016-02-20 21:33:08 +0100

    fet que es desconnecti i es connecti

--- Server/backend.js
+++ Server/backend.js
@@ -4,25 +4,41 @@
 var express = require('express');
 var app = express();
 
-var json = { "users" : [],
+//key = id, value 1r 0
+var hashmap = require('hashmap');
+
+var map = new hashmap();
+
+var all = { "users" : [],
              "mon" : []}
 
 app.post('/mon', function (req, res) {
-    var obj = JSON.parse(jsonStr);
-    obj['mon'].push({"id": req.body});
-    json = JSON.stringify(obj);
+    all['mon'].push({"id": req.body.user});
+    map.set(req.body.user,0);
 });
 
 
-app.post('/login', function (req, res) {
-    var obj = JSON.parse(jsonStr);
-    obj['users'].push({"id": req.body});
-    json = JSON.stringify(obj);
+app.post('/login', function (req, res, next) {
+    all['users'].push({"id": req.body.user});
 });
 
 
+//et passen el tag = user i el id per req.body i mirar que el ultim sigui el ++ del anterior
+app.post('/ping', function (req, res, next) {
+    if(map.has(req.body.user)) {
+        var cnt = map.get(req.body.user) + 1;
+        if(cnt  == req.body.id) {
+            console.log("All right folks");
+        }
+        else {
+            throw new Error("Has estat desconectat");
+        }
+    }
+    else throw new Error('No has començat la monitorització ' + req.body.user);
 
+});
 
+app.listen(8080);
 
 
 
