86df296e228c1b10c668beb5b260cb6344df0003
Juan Salmerón Moya <jnssterrass@gmail.com>
2016-02-21 02:37:47 +0100

    CORS

--- Server/backend.js
+++ Server/backend.js
@@ -4,6 +4,12 @@
 var express = require('express');
 var app = express();
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
