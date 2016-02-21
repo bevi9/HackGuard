1bc2234bca90ef25ea160b402af4444efc29fcb3
Juan Salmerón Moya <jnssterrass@gmail.com>
2016-02-21 02:43:17 +0100

    CORS 4 All routes

--- Server/backend.js
+++ Server/backend.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var app = express();
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
