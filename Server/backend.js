ff02b13fb501c27a7ed9142c65aeb75cb23a4611
Berta <berta.viader@est.fib.upc.edu>
2016-02-21 04:06:52 +0100

    update comptadors

--- Server/backend.js
+++ Server/backend.js
@@ -12,6 +12,7 @@ app.all('*', function(req, res, next) {
 
 var bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -21,6 +22,8 @@ var hashmap = require('hashmap');
 var Twitter = require('twitter');
 
 var map = new hashmap();
+
+var maptime = new hashmap();
 
 var all = { "users" : [],
              "mon" : []}
@@ -36,12 +39,17 @@ app.post('/mon', function (req, res) {
         res.sendStatus(200);
         console.log("okay");
         map.set(req.body.user,0);
+        maptime.set(req.body.user, Date.now());
     }
 });
 
 
 app.post('/login', function (req, res, next) {
     all['users'].push({"id": req.body.user});
+});
+
+app.post('/stop', function(req, res, next) {
+    map.remove(req.body.user);
 });
 
 
@@ -55,25 +63,28 @@ var client = new Twitter({
 
 //et passen el tag = user i el id per req.body i mirar que el ultim sigui el ++ del anterior
 app.post('/ping', function (req, res, next) {
-    res.send(req);
-    if(map.has(req.body.user)) {
+    Date.now = function() { return new Date().getTime(); };
+    if(map.has(req.body.user.toString())) {
         var cnt = map.get(req.body.user) + 1;
         if(cnt == req.body.id) {
             console.log("All right folks");
             map.remove(req.body.user);
             map.set(req.body.user,cnt);
+            maptime.set(req.body.user, Date.now());
         }
         else {
-            throw new Error("Has estat desconectat");
             client.post('statuses/update', {status: "Remember remember the fifth of november"}, function(error, tweet, response){
                 console.log(tweet);  // Tweet body.
                 console.log(response);  // Raw response object.
             });
+            throw new Error("Has estat desconectat");
         }
     }
     else throw new Error('No has començat la monitorització ' + req.body.user);
 
 });
+
+
 
 
 app.post('/twit', function (req, res, next) {
